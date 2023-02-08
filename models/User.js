88fdea47b793dfb922a1b54bc4fceeb8ecfcfc2b
Leandro Tabak <leandrogtabak@gmail.com>
2023-02-08 18:19:01 +0200

    Updated and added new characteristics

--- models/User.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export const User = mongoose.model('Users', {
-  username: String,
-  password: String,
-});
--- server.js
+++ server.js
@@ -1,120 +1,163 @@
-import express from 'express';
-import { Server as HttpServer } from 'http';
-import { Server as IOServer } from 'socket.io';
-import { ContenedorArchivo } from './contenedores/ContenedorArchivo.js';
-import { conectarDB } from './controllersdb.js';
-import handlebars from 'express-handlebars';
-import session from 'express-session';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-import { passportMiddleware } from './middlewares/passport.js';
-
-require('dotenv').config();
-import router from './routes/router.js';
-const yargs = require('yargs/yargs')(process.argv.slice(2));
-const args = yargs.argv._;
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const express = require('express');
 const app = express();
-const httpServer = new HttpServer(app);
-const io = new IOServer(httpServer);
-
-app.use(express.static(path.resolve(__dirname, './views')));
+// import { Server as HttpServer } from 'http';
+// import { Server as IOServer } from 'socket.io';
+const passport = require('passport');
+const dotenv = require('dotenv');
+dotenv.config();
+const log4js = require('./utils/logs.js');
+const MongoStore = require(`connect-mongo`);
+const session = require('express-session');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/profileImg/', express.static('./public/profileImg'));
+
+// import { ContenedorArchivo } from './contenedores/ContenedorArchivo.js';
+// import { conectarDB } from './controllersdb.js';
+// import handlebars from 'express-handlebars';
+// import session from 'express-session';
+
+// import { passportMiddleware } from './middlewares/passport.js';
+
+// import router from './routes/router.js';
+// const yargs = require('yargs/yargs')(process.argv.slice(2));
+// const args = yargs.argv._;
+
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const httpServer = new HttpServer(app);
+// const io = new IOServer(httpServer);
+
+// app.use(express.static(path.resolve(__dirname, './views')));
 
 app.use(
   session({
-    secret: 'Is this the real life? Is this just fantasy?',
-    cookie: {
-      httpOnly: false,
-      secure: false,
-      expires: 60000,
-      maxAge: parseInt(process.env.TIEMPO_EXPIRACION),
-    },
-    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.URL_MONGODB,
+      ttl: 10,
+    }),
+    secret: process.env.SESSION_SECRET,
     resave: true,
-    saveUninitialized: false,
+    saveUninitialized: true,
+    cookie: { maxAge: parseInt(process.env.TIME_EXPIRATION) },
   })
 );
 
-app.use(passportMiddleware.initialize());
-app.use(passportMiddleware.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = parseInt(args[0]) || 8080;
+const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const miContenedorMensajes = new ContenedorArchivo('./mensajes.json');
-const miContenedorProductos = new ContenedorArchivo('./productos.json');
-
-// ------------------------------------------------------------------------------
-//  HANDLEBARS
-// ------------------------------------------------------------------------------
-
-app.engine(
-  'hbs',
-  handlebars.engine({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: './views/layouts/',
-    partialsDir: './views/partials/',
-  })
-);
-
-// establecemos el motor de plantilla que se utiliza
-app.set('view engine', 'hbs');
-// establecemos directorio donde se encuentran los archivos de plantilla
-app.set('views', './views');
+app.set(`views`, `./views`);
+app.set(`view engine`, `ejs`);
 
 // ------------------------------------------------------------------------------
 //  ROUTER
 // ------------------------------------------------------------------------------
-app.use('/', router);
+// app.use('/', router);
 
 // ------------------------------------------------------------------------------
 //  SOCKET
 // ------------------------------------------------------------------------------
 
-io.on('connection', async (socket) => {
-  console.log('Un cliente se ha conectado');
+// io.on('connection', async (socket) => {
+//   console.log('Un cliente se ha conectado');
 
-  socket.on('onload', async () => {
-    const productos = await miContenedorProductos.getAll();
-    const mensajes = await miContenedorMensajes.getAll();
-    io.sockets.emit('productos', productos);
-    io.sockets.emit('mensajes', mensajes);
-  });
+//   socket.on('onload', async () => {
+//     const productos = await miContenedorProductos.getAll();
+//     const mensajes = await miContenedorMensajes.getAll();
+//     io.sockets.emit('productos', productos);
+//     io.sockets.emit('mensajes', mensajes);
+//   });
 
-  socket.on('new-message', async (newMessage) => {
-    await miContenedorMensajes.save(newMessage);
-    const mensajes = await miContenedorMensajes.getAll();
-    io.sockets.emit('mensajes', mensajes);
-  });
-  socket.on('new-product', async (newProduct) => {
-    await miContenedorProductos.save(newProduct);
-    const productos = await miContenedorProductos.getAll();
-    io.sockets.emit('productos', productos);
-  });
+//   socket.on('new-message', async (newMessage) => {
+//     await miContenedorMensajes.save(newMessage);
+//     const mensajes = await miContenedorMensajes.getAll();
+//     io.sockets.emit('mensajes', mensajes);
+//   });
+//   socket.on('new-product', async (newProduct) => {
+//     await miContenedorProductos.save(newProduct);
+//     const productos = await miContenedorProductos.getAll();
+//     io.sockets.emit('productos', productos);
+//   });
+// });
+
+const loggerConsole = log4js.getLogger(`default`);
+const loggerArchiveWarn = log4js.getLogger(`warnArchive`);
+// const loggerArchiveError = log4js.getLogger(`errorArchive`);
+
+//Loggers
+app.use((req, res, next) => {
+  loggerConsole.info(`
+    Ruta requerida: ${req.originalUrl}
+    Metodo ${req.method}`);
+  next();
 });
 
-// ------------------------------------------------------------------------------
-//  LISTEN SERVER
-// ------------------------------------------------------------------------------
+/* Middleware para comprobar si el usuario esta loggeado */
+const isLogged = (req, res, next) => {
+  let msgError = `Para acceder a esta URL debe iniciar sesión`;
+  if (req.user) {
+    next();
+  } else {
+    return res.render('viewError', { msgError });
+  }
+};
+//ROUTES
+const productsRouter = require(`./routes/productsRouter`);
+const carritoRouter = require(`./routes/cartRouter`);
+const { loginRouter } = require(`./routes/userRouter`);
+const { signupRouter } = require(`./routes/userRouter`);
+const { logoutRouter } = require(`./routes/userRouter`);
+const { profileRouter } = require(`./routes/userRouter`);
+const generalViewsRouter = require(`./routes/generalViewsRouter`);
+const ordenesRouter = require(`./routes/ordenesRouter`);
 
-conectarDB(process.env.URL_BASE_DE_DATOS, (err) => {
-  if (err) return console.log('error en conexión de base de datos', err);
-  console.log('BASE DE DATOS CONECTADA');
+app.use(`/`, generalViewsRouter);
+app.use(`/api/productos`, isLogged, productsRouter);
+app.use(`/api/carrito`, isLogged, carritoRouter);
+app.use(`/api/ordenes`, isLogged, ordenesRouter);
+app.use(`/login`, loginRouter);
+app.use(`/signup`, signupRouter);
+app.use('/logout', isLogged, logoutRouter);
+app.use(`/profile`, isLogged, profileRouter);
 
-  httpServer.listen(PORT, function () {
-    console.log(`Servidor corriendo en ${PORT}`);
-  });
+app.use((req, res) => {
+  loggerConsole.warn(`
+    Estado: 404
+    Ruta requerida: ${req.originalUrl}
+    Metodo ${req.method}`);
+
+  loggerArchiveWarn.warn(`Estado: 404, Ruta consultada: ${req.originalUrl}, Metodo ${req.method}`);
+  const msgError = `Estado: 404, Ruta consultada: ${req.originalUrl}, Metodo ${req.method}`;
+
+  res.render(`viewError`, { msgError });
 });
 
-httpServer.on('error', (error) => console.log(`Error en el servidor: ${error}`));
+const CLUSTER = Boolean(process.env.CLUSTER);
+
+const runServer = (PORT) => {
+  app.listen(PORT, () => loggerConsole.debug(`Servidor corriendo en el puerto ${PORT}`));
+};
+
+if (CLUSTER) {
+  if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on(`exit`, (worker, code, signal) => {
+      cluster.fork();
+    });
+  } else {
+    runServer(PORT);
+  }
+} else {
+  runServer(PORT);
+}
